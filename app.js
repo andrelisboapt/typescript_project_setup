311fde5f4d4ddf2ffc61b80c83d7c4967722a7f9
André Lisboa <andrelisboapt@hotmail.com>
2023-05-10 15:34:12 +0100

    object types

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2; //here does a math operation
-    if (showResult) {
-        console.log(phrase + result); //here returns a string and thats why we can't write phrase + n1 + n2
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5; //this really needs to be a number since we declared the parameters as numbers
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'André',
+    age: 30
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,19 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) { //assigning a type to the parameters, typescript uses these "static types", javascript is more like "dynamic types"
-    
-    const result = n1 + n2; //here does a math operation
-    if(showResult){
-        console.log(phrase + result); //here returns a string and thats why we can't write phrase + n1 + n2
-    }else{
-    return result;
-    }
-}
+const person:{
+    name: string;  //giving types to object properties 
+    age: number;
+} = {
+    name: 'André',
+    age: 30
+};
 
-
-
-
-const number1 = 5; //this really needs to be a number since we declared the parameters as numbers
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name);
