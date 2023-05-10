a640a8fb771c12523a2a560ee4240ae5d06db7c1
André Lisboa <andrelisboapt@hotmail.com>
2023-05-10 10:42:07 +0100

    Using Types

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Your code goes here!!!')
+function add(n1: number, n2: number) { //assigning a type to the parameters
+    return n1 + n2;
+}
+
+const number1 = 5; //this really needs to be a number since we declared the parameters as numbers
+const number2 = 2.8;
+
+const result = add(number1, number2)
+
+console.log(result)
