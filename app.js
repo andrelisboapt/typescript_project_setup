6a9559371c767ea11be16b5f4ad64d71a862b7c4
André Lisboa <andrelisboapt@hotmail.com>
2023-05-10 12:15:55 +0100

    working with the CORE types

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2; //here does a math operation
+    if (showResult) {
+        console.log(phrase + result); //here returns a string and thats why we can't write phrase + n1 + n2
+    }
+    else {
+        return result;
+    }
 }
-var number1 = '5';
+var number1 = 5; //this really needs to be a number since we declared the parameters as numbers
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-function add(n1: number, n2: number) { //assigning a type to the parameters
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) { //assigning a type to the parameters, typescript uses these "static types", javascript is more like "dynamic types"
+    
+    const result = n1 + n2; //here does a math operation
+    if(showResult){
+        console.log(phrase + result); //here returns a string and thats why we can't write phrase + n1 + n2
+    }else{
+    return result;
+    }
 }
+
+
+
 
 const number1 = 5; //this really needs to be a number since we declared the parameters as numbers
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2)
-
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
