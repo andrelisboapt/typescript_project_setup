47d04987fc38bbbf3e2518d292b9a65378f1b440
André Lisboa <andrelisboapt@hotmail.com>
2023-05-11 12:01:56 +0100

    Arrays Types

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var person = {
+var person /* :{
+    name: string;  //giving types to object properties
+    age: number;
+}  */ = {
     name: 'André',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities; //here we are saying that this array is an array of strings, if we want to mix types, we can use "any[]"
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); //here we are certain that "hobby" is string since our array is an array of strings
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-const person:{
+const person/* :{
     name: string;  //giving types to object properties 
     age: number;
-} = {
+}  */= {
     name: 'André',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[]; //here we are saying that this array is an array of strings, if we want to mix types, we can use "any[]"
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase()); //here we are certain that "hobby" is string since our array is an array of strings
+}
