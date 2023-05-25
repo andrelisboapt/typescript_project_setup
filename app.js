9ede4e5271aeee72b5b2ee3811b2196117370128
André Lisboa <andrelisboapt@hotmail.com>
2023-05-25 18:28:36 +0100

    Unknown and Never types

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ if (typeof userInput === 'string') {
 function generateError(message, code) {
     throw { message: message, errorCode: code };
 }
-var result = generateError('An error occurred!', 500);
-console.log(result);
+generateError('An error occurred!', 500);
